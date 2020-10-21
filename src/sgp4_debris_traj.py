b3650e105efc391f707a6d4e0662e6a9019346d9
smallpondtom <usasmpdtom@gmail.com>
2020-10-21 19:10:03 -0400

    Edit the sgp4 python file

--- src/sgp4_debris_traj.py
+++ src/sgp4_debris_traj.py
@@ -1,3 +1,12 @@
+"""
+ ___________   _________     _____________   ______________    ______            ______
+|     ______| |   ___   >\  |_____   _____| |_____    _____|  /< __ >\          /> __ <\
+|    |______  |  |   \   >\      |   |           |    |      /> |__| <\        /< |__| >\
+|_______    | |  |    |  >|      |   |           |    |     /< ______ >\      /> ______ <\
+ ______|    | |  |___/   >/  ____|   |____       |    |    /> |      | <\    /< |      | >\
+|___________| |_________>/  |_____________|      |____|   /<__|      |__>\  />__|      |__<\
+
+"""
 from sgp4.api import jday, Satrec, SatrecArray
 import numpy as np
 import pandas as pd
@@ -14,11 +23,11 @@ class SGP4PREDICT:
     Class to predict the space debris trajectory using the sgp4 algorithm for a specified date
     """
 
-    def __init__(self, tledata: List[str]=None,
+    def __init__(self, tledata: List[str] = None,
                  linespertle=3,
-                 start_date: str=str(datetime.datetime.utcnow()),
-                 end_date: str=None,
-                 interval: str="D") -> None:
+                 start_date: str = str(datetime.datetime.utcnow()),
+                 end_date: str = None,
+                 interval: str = "D") -> None:
         """
         Initialize parameters for class (timezone is UTC Gregorian)
         :param tledata: the TLE data input
@@ -41,7 +50,6 @@ class SGP4PREDICT:
         self._position = None
         self._velocity = None
 
-
     def get_dates(self) -> List[str]:
         """
         Function that gets dates based on start date, end date, and interval
@@ -56,7 +64,7 @@ class SGP4PREDICT:
         assert isinstance(time_list, object), "Is not object"
         return time_list
 
-    @ staticmethod
+    @staticmethod
     def convert2JulianDate(date_list: List[str]) -> Tuple[NDArray, NDArray]:
         """
         Function that creates a list of Julian Dates and decimals from the UTC date list
@@ -91,7 +99,7 @@ class SGP4PREDICT:
 
         for i in range(0, len(self.tledata), self.linespertle):
             line1 = self.tledata[i + m]
-            line2 = self.tledata[i + (m+1)]
+            line2 = self.tledata[i + (m + 1)]
 
             # Check to see if the Elset Classification is 'U' indicating that it is an
             # unclassified object, that is, a space debris
@@ -136,3 +144,54 @@ class SGP4PREDICT:
             os.remove(file_name)
         with open(file_name, 'w') as jfile:
             json.dump(res_dict, jfile, indent=4)
+
+
+def main():
+    TLE_sample = ['0 VANGUARD 1',
+                  '1     5U 58002B   20270.72609959 +.00000008 +00000-0 +35143-4 0  9996',
+                  '2     5 034.2447 276.2975 1845776 088.0693 292.9082 10.84868479216254',
+                  '0 VANGUARD 2',
+                  '1    11U 59001A   20270.83057634 -.00000014 +00000-0 -52792-5 0  9991',
+                  '2    11 032.8687 034.7609 1467087 103.7620 273.0083 11.85679410287012',
+                  '0 VANGUARD R/B',
+                  '1    12U 59001B   20270.89554473 +.00000372 +00000-0 +19820-3 0  9999',
+                  '2    12 032.9137 356.0609 1666130 249.2921 092.1676 11.44320987291609',
+                  '0 VANGUARD R/B',
+                  '1    16U 58002A   20270.85942587 -.00000022 +00000-0 -46274-4 0  9994',
+                  '2    16 034.2702 221.8316 2022572 305.2573 037.4791 10.48675665469485',
+                  '0 VANGUARD 3',
+                  '1    20U 59007A   20270.48712837 +.00000328 +00000-0 +14121-3 0  9999',
+                  '2    20 033.3411 182.1436 1666591 082.6853 295.9308 11.55733657242298',
+                  '0 EXPLORER 7',
+                  '1    22U 59009A   20270.68850821 +.00000323 +00000-0 +46901-4 0  9998',
+                  '2    22 050.2861 347.0443 0139436 285.4847 073.0739 14.95205750485528',
+                  '0 TIROS 1',
+                  '1    29U 60002B   20270.51303602 -.00000101 +00000-0 +16003-4 0  9997',
+                  '2    29 048.3794 191.6811 0024581 304.4255 055.4334 14.74372853233453',
+                  '0 TRANSIT 2A',
+                  '1    45U 60007A   20270.87697544 -.00000081 +00000-0 +70910-5 0  9995',
+                  '2    45 066.6929 113.6127 0262116 242.5192 114.9042 14.33687614080375',
+                  '0 SOLRAD 1 (GREB)',
+                  '1    46U 60007B   20270.90681461 +.00000039 +00000-0 +31064-4 0  9999',
+                  '2    46 066.6896 132.4066 0203897 054.4060 307.5857 14.49317409153818',
+                  '0 THOR ABLESTAR R/B',
+                  '1    47U 60007C   20270.67888321 -.00000040 +00000-0 +16368-4 0  9991',
+                  '2    47 066.6644 244.3196 0233780 230.3761 127.6505 14.42103295149279',
+                  '0 DELTA 1 R/B',
+                  '1    50U 60009B   20270.45814876 -.00000089  00000-0 -40244-4 0  9991',
+                  '2    50  47.2307 167.7862 0113683 266.9466  91.8310 12.20112284683000',
+                  '0 ECHO 1 DEB (METAL OBJ)',
+                  '1    51U 60009C   20270.77783235 -.00000155 +00000-0 -66528-3 0  9997',
+                  '2    51 047.2148 033.9443 0107347 105.2138 256.0549 12.18276304676444',
+                  '0 COSMOS 86',
+                  '1  1584U 65073A   20270.84441890 -.00000096 +00000-0 -90198-6 0  9998',
+                  '2  1584 056.0607 043.0461 0211763 335.4680 193.5629 12.51876101515807', ]
+
+    # Add custom date to conduct prediction
+    ti = "2020-10-11 00:02:30"
+    tf = "2020-12-12 14:02:30"
+    sgp4sim = SGP4PREDICT(tledata=TLE_sample, linespertle=3, start_date=ti, end_date=tf, interval="D")
+    r, v = sgp4sim.run_sgp4()
+    # sgp4sim.export_json() # test to see if data can be retrieved properly
+
+if __name__ == '__main__': main()
